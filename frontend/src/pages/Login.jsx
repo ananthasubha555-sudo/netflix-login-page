c2fcb0e3abbb14359d20c7cce1472d6369a4e203
ananthasubha555-sudo <ananthasubha555@gmail.com>
2025-12-11 20:45:23 +0530

    Change login API endpoint to localhost

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -13,7 +13,7 @@ export default function Login() {
     e.preventDefault();
 
     try {
-      const res = await axios.post("https://netflix-login-page-gapq.onrender.com", {
+      const res = await axios.post("http://localhost:5000/login", {
         email,
         password,
       });
@@ -70,4 +70,5 @@ export default function Login() {
     </div>
   );
 }
+
 
